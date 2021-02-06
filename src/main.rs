52211fe247f145d49c0ac7f8c443b4e01291ea43
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-02-06 15:29:05 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,31 @@ use deeplll::{deep_lll, s2_lll};
 use parse::matrix_parse;
 
 use ndarray::prelude::*;
+use rug::Rational;
 use std::path::Path;
+use std::fs::File;
+use std::io::prelude::*;
 
 use std::time::{Instant};
 
 macro_rules! measure {
-  ($str: expr, $x: expr) => {
+  ($str: expr, $result_f: expr, $x: expr) => {
     {
       let start = Instant::now();
       let result = $x;
       let end = start.elapsed();
-      println!("{}: {}.{:03} sec", $str, end.as_secs(), end.subsec_nanos() / 1_000_000);
+      write!($result_f, "{}: {}.{:03} sec", $str, end.as_secs(), end.subsec_nanos() / 1_000_000)?;
       result
     }
   };
+}
+
+fn experiment(b: Array2<Rational>, result_path_str: &str) -> std::io::Result<()> {
+  let mut result_f = File::create(result_path_str)?;
+  let (new_b, v, mu, hist, cnt) =
+    measure!(result_path_str, result_f, deep_lll(b.slice(s![0..10, 0..10]).to_owned(), 1.into(), true, 100));
+  writeln!(result_f, "b: {:?}\nv_norms: {:?}\n{:?}\n(hist.len, cnt): {:?}\n{:?}", new_b, v, mu, (hist.len(), cnt), hist)?;
+  Ok(())
 }
 
 fn main() {
@@ -26,7 +37,5 @@ fn main() {
   let path = Path::new(path_str);
   let b = matrix_parse(path);
 
-  let (new_b, v, mu, hist, cnt) =
-    measure!(path_str, deep_lll(b.slice(s![0..30, 0..30]).to_owned(), 1.into(), true, 100));
-  println!("b: {:?}\nv_norms: {:?}\n{:?}\n(hist.len, cnt): {:?}\n{:?}", new_b, v, mu, (hist.len(), cnt), hist);
+  experiment(b, "results/svp/svpchallengedim40seed0.txt").unwrap();
 }
