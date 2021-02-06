6993dd4f581c8071ec842ae1edd61c2377d17de7
bababax11 <38175513+bababax11@users.noreply.github.com>
2021-02-06 16:03:52 +0900

    refactor for experiments

--- src/main.rs
+++ src/main.rs
@@ -18,24 +18,36 @@ macro_rules! measure {
       let start = Instant::now();
       let result = $x;
       let end = start.elapsed();
-      write!($result_f, "{}: {}.{:03} sec", $str, end.as_secs(), end.subsec_nanos() / 1_000_000)?;
+      writeln!($result_f, "{}: {}.{:03} sec", $str, end.as_secs(), end.subsec_nanos() / 1_000_000)?;
       result
     }
   };
 }
 
-fn experiment(b: Array2<Rational>, result_path_str: &str) -> std::io::Result<()> {
+fn experiment_mat(mat_path_str: &str) {
+  let path = Path::new(mat_path_str);
+  let b = matrix_parse(path);
+  for ndim in &[15, 20, 25, 30, 35] {
+    let path_str_base = mat_path_str.split("/").last().unwrap().split(".").next().unwrap();
+    for rat in &[Rational::from(1), Rational::from((99, 100))] {
+      let result_path_str = format!("{}{}dim{}delta{}.txt", "results/svp/", path_str_base, *ndim, rat.to_f32());
+      eprintln!("\n{}", &result_path_str);
+      experiment_unit(b.slice(s![0..(*ndim), 0..(*ndim)]), &result_path_str, rat.to_owned()).unwrap();
+    }
+  }
+}
+
+fn experiment_unit(b: ArrayView2<Rational>, result_path_str: &str, delta: Rational) -> std::io::Result<()> {
   let mut result_f = File::create(result_path_str)?;
   let (new_b, v, mu, hist, cnt) =
-    measure!(result_path_str, result_f, deep_lll(b.slice(s![0..10, 0..10]).to_owned(), 1.into(), true, 100));
+    measure!(result_path_str, result_f, deep_lll(b.to_owned(), delta, true, 100));
   writeln!(result_f, "b: {:?}\nv_norms: {:?}\n{:?}\n(hist.len, cnt): {:?}\n{:?}", new_b, v, mu, (hist.len(), cnt), hist)?;
   Ok(())
 }
 
 fn main() {
-  let path_str = "matrices/svp/svpchallengedim40seed0.txt";
-  let path = Path::new(path_str);
-  let b = matrix_parse(path);
-
-  experiment(b, "results/svp/svpchallengedim40seed0.txt").unwrap();
+  for i in 0..5 {
+    let mat_path_str = format!("matrices/svp/svpchallengedim40seed{}.txt", i);
+    experiment_mat(&mat_path_str);
+  }
 }
