23be1c8eb216c741f2f0549802ae783aeeb48bce
Odagawa <38175513+bababax11@users.noreply.github.com>
2021-02-06 18:24:13 +0900

    implement potlll

--- src/deeplll/mod.rs
+++ src/deeplll/mod.rs
@@ -129,6 +129,19 @@ fn max_sjk(v: &Array1<Rational>, mu: &Mu, k: usize) -> (usize, Rational) {
   let (e_, max_val) = argmax(&sjk);
   (k - e_ - 1, max_val)
 }
+fn min_pot(v: &Array1<Rational>, mu: &Mu, k: usize) -> (usize, Rational) {
+  let mut potjk = vec![Rational::new(); k];
+  let mut before = Rational::from(1);
+  let mut pjbk = v[k].clone();
+  for (e, j) in (0..k).rev().enumerate() {
+    let tmp = mu[(k, j)].clone().pow(2) * &v[j];
+    pjbk += &tmp;
+    before *= pjbk.clone() / &v[j];
+    potjk[e] = before.clone();
+  }
+  let (e_, min_val) = argmin(&potjk);
+  (k - e_ - 1, min_val)
+}
 
 fn argmax<T: PartialOrd + Clone>(xs: &[T]) -> (usize, T) {
   if xs.is_empty() {
@@ -147,9 +160,34 @@ fn argmax<T: PartialOrd + Clone>(xs: &[T]) -> (usize, T) {
     (max_ix, max_val.clone())
   }
 }
+fn argmin<T: PartialOrd + Clone>(xs: &[T]) -> (usize, T) {
+  if xs.is_empty() {
+    panic!();
+  } else if xs.len() == 1 {
+    (0, xs[0].clone())
+  } else {
+    let mut min_val = &xs[0];
+    let mut min_ix = 0;
+    for (i, x) in xs.iter().enumerate().skip(1) {
+      if x < min_val {
+        min_val = x;
+        min_ix = i;
+      }
+    }
+    (min_ix, min_val.clone())
+  }
+}
 #[inline]
 pub fn ss(v: &Array1<Rational>) -> Rational {
   v.fold(Rational::new(), |sum, e| sum + e)
+}
+pub fn pot(v: &Array1<Rational>) -> Rational {
+  let mut p = Rational::from(1);
+  let n = v.len();
+  for (i, r) in v.iter().enumerate() {
+    p *= Rational::from(r.pow((n - i) as u32));
+  }
+  p
 }
 
 pub fn s2_lll(
@@ -195,6 +233,49 @@ pub fn s2_lll(
   (b, v, mu, hist, cnt)
 }
 
+pub fn pot_lll(
+  mut b: Array2<Rational>,
+  delta: Rational,
+  verbose: bool,
+  verbose_count: usize,
+) -> (
+  Array2<Rational>,
+  Array1<Rational>,
+  Mu,
+  Vec<(usize, usize)>,
+  usize,
+) {
+  let n = b.nrows();
+  let mut hist = Vec::with_capacity(verbose_count * 100);
+  let mut k = 1;
+  let (mut v, mut mu) = orthogonize(&b);
+
+  let mut cnt = 0;
+
+  while k < n {
+    cnt += 1;
+    if verbose && cnt % verbose_count == 0 {
+      eprint!("=");
+    }
+    let (_b, _mu) = size_reduce(&b, &mu);
+    b = _b;
+    mu = _mu;
+
+    let (i, p) = min_pot(&v, &mu, k);
+    if &delta <= &p {
+      k += 1;
+    } else {
+      hist.push((i, k));
+      swap(b.view_mut(), i, k);
+      let (_v, _mu) = orthogonize(&b);
+      v = _v;
+      mu = _mu;
+      k = usize::max(i, 1);
+    }
+  }
+  (b, v, mu, hist, cnt)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -211,6 +292,7 @@ mod tests {
       [rat!(1), rat!(0), rat!(1)]
     ];
     let (v, m) = orthogonize(&b);
+    assert_eq!(pot(&v), rat!(24));
     assert_eq!(v, array![rat!(2), rat!(3, 2), rat!(4, 3)]);
     assert_eq!(m[(1, 0)], rat!(1, 2));
     assert_eq!(m[(2, 0)], rat!(1, 2));
@@ -282,5 +364,25 @@ mod tests {
     assert_eq!(mu2[(2, 0)], rat!(0));
     assert_eq!(mu2[(2, 1)], rat!(18, 53));
     assert_eq!(&hist, &[(1, 2, rat!(59041, 1958)), (0, 2, rat!(1651, 66)), (0, 2, rat!(2239, 90))]);
+  }
+  #[test]
+  fn pot_lll_test() {
+    let b = array![
+      [rat!(3), rat!(1), rat!(-1)],
+      [rat!(-3), rat!(-2), rat!(-3)],
+      [rat!(1), rat!(-1), rat!(2)]
+    ];
+    let (b2, v2, mu2, _, _) = pot_lll(b, rat!(1), false, 1);
+    assert_eq!(b2, array![
+      [rat!(1), rat!(-1), rat!(2)],
+      [rat!(1), rat!(-2), rat!(-2)],
+      [rat!(3), rat!(1), rat!(-1)]
+    ]);
+    assert_eq!(v2, array![
+      rat!(6), rat!(53, 6), rat!(529, 53)
+    ]);
+    assert_eq!(mu2[(1, 0)], rat!(-1, 6));
+    assert_eq!(mu2[(1, 1)], rat!(0));
+    assert_eq!(mu2[(1, 2)], rat!(18, 53));
   }
 }
